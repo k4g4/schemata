0ce9b8a4b8f431fd7eb3717494d1d430263d54a6
Andres Dejesus <andresdejesus123@gmail.com>
2024-10-29 21:18:26 -0500

    before big refactor

--- src/items.rs
+++ src/items.rs
@@ -1,8 +1,38 @@
+use rustc_hash::FxHashMap;
+
 use crate::error::Error;
 use std::{
     fmt, mem,
     ops::{Add, Div, Mul, Sub},
+    ptr,
 };
+
+#[derive(Debug)]
+pub struct Scope {
+    parent: *const Scope,
+    vars: FxHashMap<String, Item>,
+    funcs: FxHashMap<String, Vec<Item>>,
+}
+
+impl Scope {
+    fn lookup(&self, var: &str) -> Option<&Item> {
+        // SAFETY: The returned item has lifetime 'self and the parent has lifetime 'self
+        // since it belongs to this scope, so aliasing rules can't be violated.
+        self.vars
+            .get(var)
+            .or_else(|| unsafe { self.parent.as_ref() }.and_then(|parent| parent.lookup(var)))
+    }
+}
+
+impl Default for Scope {
+    fn default() -> Self {
+        Self {
+            parent: ptr::null(),
+            vars: Default::default(),
+            funcs: Default::default(),
+        }
+    }
+}
 
 #[derive(Copy, Clone, Debug)]
 pub enum Builtin {
@@ -10,25 +40,23 @@ pub enum Builtin {
     Sub,
     Mul,
     Div,
-    Define,
 }
 
 impl Builtin {
-    pub fn eval(self, cdr: &[Item]) -> Result<Item, Error> {
+    pub fn eval(self, tail: &[Item]) -> Result<Item, Error> {
         if matches!(self, Self::Add | Self::Sub | Self::Mul | Self::Div) {
-            if matches!(self, Self::Div) && cdr.iter().any(|item| matches!(item, Item::Num(0.0))) {
+            if matches!(self, Self::Div) && tail.iter().any(|item| matches!(item, Item::Num(0.0))) {
                 return Err(Error::Other("Can't divide by zero!".into()));
             };
-            if !cdr.iter().all(|item| matches!(item, Item::Num(_))) {
+            if !tail.iter().all(|item| matches!(item, Item::Num(_))) {
                 return Err(Error::Other(format!("{self} can't operate on non-number")));
             }
-            if let Some((&Item::Num(car), cdr)) = cdr.split_first() {
+            if let Some((&Item::Num(head), tail)) = tail.split_first() {
                 let op = match self {
                     Self::Add => <_ as Add>::add,
                     Self::Sub => <_ as Sub>::sub,
                     Self::Mul => <_ as Mul>::mul,
                     Self::Div => <_ as Div>::div,
-                    _ => unreachable!(),
                 };
                 let get_num = |item| {
                     if let &Item::Num(n) = item {
@@ -37,7 +65,7 @@ impl Builtin {
                         None
                     }
                 };
-                Ok(Item::Num(cdr.iter().flat_map(get_num).fold(car, op)))
+                Ok(Item::Num(tail.iter().flat_map(get_num).fold(head, op)))
             } else if matches!(self, Self::Add | Self::Mul) {
                 Ok(Item::Num(0.0))
             } else {
@@ -45,8 +73,6 @@ impl Builtin {
                     "Wrong number of arguments for {self}"
                 )))
             }
-        } else if matches!(self, Self::Define) {
-            todo!()
         } else {
             unreachable!("Unsupported builtin: {self}")
         }
@@ -60,33 +86,48 @@ impl fmt::Display for Builtin {
             Builtin::Sub => write!(f, "-"),
             Builtin::Mul => write!(f, "*"),
             Builtin::Div => write!(f, "/"),
-            Builtin::Define => write!(f, "define"),
         }
     }
 }
 
 #[derive(Clone, Debug)]
 pub enum Item {
+    Define,
     Num(f64),
+    Ident(String),
     Builtin(Builtin),
     List(Vec<Item>),
 }
 
 impl Item {
-    pub fn eval(self) -> Result<Self, Error> {
+    pub fn eval(self, scope: &mut Scope) -> Result<Self, Error> {
         let Self::List(mut list) = self else {
             return Ok(self);
         };
-        let Some((car, cdr)) = list.split_first_mut() else {
+        let Some((head, tail)) = list.split_first_mut() else {
             return Ok(Default::default());
         };
-        *car = mem::take(car).eval()?;
-        for item in &mut *cdr {
-            *item = mem::take(item).eval()?;
-        }
-        match car {
-            Self::Builtin(builtin) => builtin.eval(cdr),
-            _ => Ok(Self::List(list)),
+        *head = mem::take(head).eval(scope)?;
+        match head {
+            Self::Define => match tail {
+                [] => Err(Error::Other("Empty define".into())),
+                [Self::Ident(ident), value] => {
+                    let value = mem::take(value).eval(scope)?;
+                    todo!()
+                }
+                [Self::List(signature), _, ..] => todo!(),
+                _ => Err(Error::Other("Invalid define".into())),
+            },
+            _ => {
+                for item in &mut *tail {
+                    *item = mem::take(item).eval(scope)?;
+                }
+                if let Self::Builtin(builtin) = head {
+                    builtin.eval(tail)
+                } else {
+                    Ok(Self::List(list))
+                }
+            }
         }
     }
 }
@@ -109,18 +150,18 @@ impl fmt::Display for Item {
         indent(f)?;
 
         match self {
+            Self::Define => f.write_str("define\n"),
             Self::Num(num) => writeln!(f, "{num}"),
+            Self::Ident(ident) => writeln!(f, "{ident}"),
             Self::Builtin(builtin) => writeln!(f, "{builtin}"),
-            Self::List(items) if items.is_empty() => {
-                writeln!(f, "()")
-            }
+            Self::List(items) if items.is_empty() => f.write_str("()\n"),
             Self::List(items) => {
-                writeln!(f, "(")?;
+                f.write_str("(\n")?;
                 for item in items {
                     write!(f, "{item:width$}", width = width + 1)?;
                 }
                 indent(f)?;
-                writeln!(f, ")")
+                f.write_str(")\n")
             }
         }
     }
--- src/parse.rs
+++ src/parse.rs
@@ -4,12 +4,13 @@ use crate::{
 };
 use nom::{
     branch::alt,
-    character::complete::{char, multispace0},
-    combinator::value,
+    bytes::complete::{tag, take_till1},
+    character::complete::{char, multispace0, one_of},
+    combinator::{into, map_res, not, value},
     error::context,
     multi::many0,
     number::complete::double,
-    sequence::delimited,
+    sequence::{delimited, preceded},
     Parser,
 };
 use std::fmt::Write;
@@ -29,7 +30,7 @@ pub fn repl(mut input: &I) -> Result<(), Error> {
                 println!("{display}");
 
                 println!("Evaluated:");
-                match item.eval() {
+                match item.eval(&mut Default::default()) {
                     Ok(item) => println!("{item}"),
                     Err(error) => {
                         return Err(Error::WithContext {
@@ -52,13 +53,23 @@ pub fn repl(mut input: &I) -> Result<(), Error> {
 }
 
 fn expr(input: &I) -> ParseRes<Item> {
-    let item = alt((
-        context("list", list).map(Item::List),
+    let items = (
+        value(Item::Define, tag("define")),
+        double.map(Item::Num),
+        context("identifier", ident).map(Item::Ident),
         context("builtin", builtin).map(Item::Builtin),
-        context("number", double).map(Item::Num),
-    ));
-    let (input, item) = delimited(multispace0, item, multispace0)(input)?;
-    Ok((input, item))
+        context("list", list).map(Item::List),
+    );
+    delimited(multispace0, alt(items), multispace0)(input)
+}
+
+fn ident(input: &I) -> ParseRes<String> {
+    let delims = "();\"'`|[]{}";
+    let ident = preceded(
+        not(one_of::<_, _, Error>("#,").or(one_of(delims))),
+        take_till1(|c| delims.as_bytes().contains(&c)),
+    );
+    map_res(into(ident), String::from_utf8)(input)
 }
 
 fn builtin(input: &I) -> ParseRes<Builtin> {
