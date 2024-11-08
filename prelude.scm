18de8a860d480fd1166825ca94677329b7f3cfed
Andres Dejesus <andresdejesus123@gmail.com>
2024-11-07 20:19:48 -0600

    various prelude functions

--- prelude.scm
+++ prelude.scm
@@ -15,13 +15,16 @@
 ;;    (not (> x y)))
 
 (define (even? x) (= (remainder x 2) 0))
+(define (odd? x) (= (remainder x 2) 1))
+(define (zero? x) (= x 0))
 
 (define (square x) (* x x))
 (define (cube x) (* x x x))
-(define (pow x p)
-    (define (pow_ res p)
-        (if (= p 0) res (pow_ (* res x) (- p 1))))
-    (pow_ 1 p))
+
+(define (expt base power)
+    (define (expt_ res p)
+        (if (= p 0) res (expt_ (* res base) (- p 1))))
+    (expt_ 1 power))
 
 (define (sqrt x)
     (define e .00001)
@@ -52,3 +55,33 @@
     (if (= b 0)
         a
         (gcd b (remainder a b))))
+
+(define (length list)
+    (define (length_ at count)
+        (if (null? at)
+            count
+            (length_ (cdr at) (+ count 1))))
+    (length_ list 0))
+
+(define (map proc list)
+    (if (null? list)
+        list
+        (cons
+            (proc (car list))
+            (map proc (cdr list)))))
+
+(define (filter pred list)
+    (if (null? (cdr list))
+        list
+        (let ((rest (filter pred (cdr list))))
+            (if (pred (car list))
+                (cons (car list) rest)
+                rest))))
+
+(define (reduce combiner list)
+    (if (null? (cdr list))
+        (car list)
+        (combiner
+            (car list)
+            (reduce combiner (cdr list)))))
+
